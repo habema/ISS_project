ea08df6a9ac2d2c79e093503cf13098c64e02aa8
habualhaj <abualhaj02@gmail.com>
2023-12-23 22:04:18 +0300

    likelihood function

--- bin/Ceaser.py
+++ bin/Ceaser.py
@@ -1,5 +1,7 @@
 from string import ascii_lowercase
 import pandas as pd
+import string
+from collections import Counter
 
 ENGLISH_FREQ = {'e': 12.02, 't': 9.10, 'a': 8.12, 'o': 7.68, 'i': 7.31, 'n': 6.95,
                 's': 6.28, 'r': 6.02, 'h': 5.92, 'd': 4.32, 'l': 3.98, 'u': 2.88,
@@ -31,15 +33,36 @@ def freq_count(ct):
 #     return df
 
 
-def calculate_likelihood(plaintext):
-    frequency = freq_count(plaintext)
-    total_chars = sum([f[1] for f in frequency])
-    likelihood = 0
-    for char, freq in frequency:
-        if char in ENGLISH_FREQ:
-            print((100 * freq / total_chars))
-            likelihood += abs((100 * freq / total_chars) - ENGLISH_FREQ[char])
-    return max(0, 100 - likelihood)  
+# def calculate_likelihood(plaintext):
+#     frequency = freq_count(plaintext)
+#     total_chars = sum([f[1] for f in frequency])
+#     likelihood = 0
+#     for char, freq in frequency:
+#         if char in ENGLISH_FREQ:
+#             print((100 * freq / total_chars))
+#             likelihood += abs((100 * freq / total_chars) - ENGLISH_FREQ[char])
+#     return max(0, 100 - likelihood)  
+
+def letter_frequency(text):
+    cleaned_text = ''.join(char.lower() for char in text if char.isalpha())
+    frequencies = Counter(cleaned_text)
+    total_letters = sum(frequencies.values())
+    
+    for char in string.ascii_lowercase:
+        if char not in frequencies:
+            frequencies[char] = 0
+    
+    distribution = {letter: 100 * count / total_letters for letter, count in frequencies.items()}
+    return distribution
+
+
+def ct_likelihood(text):
+    dist1 = letter_frequency(text)
+    letters = set(ENGLISH_FREQ.keys()) | set(dist1.keys())
+    mse = sum((ENGLISH_FREQ.get(letter, 0) - dist1.get(letter, 0))**2 for letter in letters) / len(letters)
+    mse = 100 - mse
+    return round(100*((mse - 65) / (100 - 65)), 2)
+
 
 def detect_shift(freq):
     return abs(ord('e') - ord(freq[0][1]))
@@ -66,7 +89,10 @@ def decryptCeaser(ct, shift):
         c = ord(l) - ord('a')
         c = (c - shift) % 26
         message += ascii_lowercase[c]
-    like = calculate_likelihood(message)
+    if len(message) < 30:
+        like = "Message too short"
+    else:
+        like = ct_likelihood(message)
     return message, like
 
 
